2aa61ebe279454c3bdb2e637a75b9577d24007d3
kartashov.adm <igorek6500@gmaol.com>
2024-03-30 13:41:08 +0300

    preview 0.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+function createCard(data, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template');
+    const cardElement = cardTemplate.content.cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: DOM узлы
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(data);
+    });
 
-// @todo: Функция создания карточки
+    return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function renderCards(cards) {
+    const placesList = document.querySelector('.places__list');
+    placesList.innerHTML = '';
+    cards.forEach((cardData) => {
+        const cardElement = createCard(cardData, handleCardDelete);
+        placesList.appendChild(cardElement);
+    });
+}
 
-// @todo: Вывести карточки на страницу
+
+function handleCardDelete(cardData) {
+    console.log('Deleting card:', cardData);
+    const indexCard = initialCards.indexOf(cardData);
+    initialCards.splice(indexCard, 1);
+    renderCards(initialCards);
+}
+
+renderCards(initialCards);
