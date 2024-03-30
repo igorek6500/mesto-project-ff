f605c5449199bd9967837530a7dd346bb59e6844
kartashov.adm <igorek6500@gmaol.com>
2024-03-30 16:07:57 +0300

    review 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 function createCard(data, deleteCallback) {
-    const cardTemplate = document.querySelector('#card-template');
-    const cardElement = cardTemplate.content.cloneNode(true);
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     const deleteButton = cardElement.querySelector('.card__delete-button');
@@ -8,24 +8,20 @@ function createCard(data, deleteCallback) {
     cardImage.src = data.link;
     cardImage.alt = data.name;
     cardTitle.textContent = data.name;
-    deleteButton.addEventListener('click', () => {
-        deleteCallback(data);
+    deleteButton.addEventListener('click', function () {
+        const listItem = deleteButton.closest('.card');
+        deleteCallback(listItem);
     });
 
     return cardElement;
 }
 
-function handleCardDelete(cardData) {
-    const indexCard = initialCards.indexOf(cardData);
-    if (indexCard !== -1) {
-        initialCards.splice(indexCard, 1);
-        renderCards(initialCards);
-    }
+function handleCardDelete(listItem) {
+    listItem.remove();
 }
 
 function renderCards(cards) {
     const placesList = document.querySelector('.places__list');
-    placesList.innerHTML = '';
     cards.forEach((cardData) => {
         const cardElement = createCard(cardData, handleCardDelete);
         placesList.appendChild(cardElement);
